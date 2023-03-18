ab77b1acccc334ff758bfed2619ec7ffb4e98ea2
YassCryptos <yass.cryptos@gmail.com>
2023-03-17 18:57:05 -0700

    Task2PrintAplphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,11 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
  * main - Lowercase & Uppercase
  *
- * Return: 0 (Success)
+ * Return: End of the programme
  *
  */
 
@@ -12,10 +14,14 @@ int main(void)
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
+	}
 	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
 		putchar(ch);
-	putchar('\n');
+	}
+	
 
 	return (0);
 }
