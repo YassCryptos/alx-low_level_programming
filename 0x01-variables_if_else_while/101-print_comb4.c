3faf58a08611773bbde46c02189c0494a1168187
YassCryptos <yass.cryptos@gmail.com>
2023-03-17 20:04:37 -0700

    101PrintCombo4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 101 Combo4
+ */
+
+int main(void)
+{
+	int first;
+	int second;
+	int third;
+
+	for (first = 0; first < 8; first++)
+	{
+		for (second = first + 1; second < 9; second++)
+		{
+			for (third = second + 1; third < 10; third++)
+			{
+				putchar(first + '0');
+				putchar(second + '0');
+				putchar(third + '0');
+				if (first < 7 || second < 8 || third < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
